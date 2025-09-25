5b095c21b5fde344b2a14c5f47cd224c31f19def
Luca Amoriello <luca.amoriello@hotmail.it>
2025-09-25 12:04:39 +0200

    Add stripe config and local db exception

--- medusa-config.ts
+++ medusa-config.ts
@@ -3,6 +3,22 @@ import { loadEnv, defineConfig } from '@medusajs/framework/utils'
 loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
 module.exports = defineConfig({
+  modules: [
+      {
+        resolve: "@medusajs/medusa/payment",
+        options: {
+          providers: [
+            {
+              resolve: "@medusajs/medusa/payment-stripe",
+              id: "stripe",
+              options: {
+                apiKey: process.env.STRIPE_API_KEY,
+              },
+            },
+          ],
+        },
+      },
+    ],
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
     http: {
@@ -11,6 +27,10 @@ module.exports = defineConfig({
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+    },
+    databaseDriverOptions: {
+      ssl: false,
+      sslmode: "disable",
     }
   }
 })
